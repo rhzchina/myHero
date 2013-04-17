5502547e12c5e3939299518b5930e4dc86a4be70
rhzchina <77620179@qq.com>
2013-04-18 02:20:30 +0800

    chage the domain

--- Resources/GameLuaScript/Config/base.lua
+++ Resources/GameLuaScript/Config/base.lua
@@ -2,8 +2,8 @@ CONFIG_SCREEN_WIDTH  = 480
 CONFIG_SCREEN_HEIGHT = 854
 
 
-CONFIG_HOST = "http://www.xudanjie.com:1995/"
-CONFIG_HTTP_URL = "http://www.xudanjie.com:1995/"
+CONFIG_HOST = "http://www.xudanjie.com:3325/"
+CONFIG_HTTP_URL = "http://www.xudanjie.com:3325/"
 CONFIG_SOCKET_HOST = "169.254.28.29"
 CONFIG_SOCKET_PORT = 1234
 
